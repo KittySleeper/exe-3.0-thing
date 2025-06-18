3ead6e2d4d6354a6772d1a4cb135cc5c33c44bee
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-06-18 15:39:25 +0000

    duhhhhh

--- setup/windows.bat
+++ setup/windows.bat
@@ -6,16 +6,22 @@ echo Installing necessary libraries. Please wait...
 echo.
 haxelib install tjson --quiet
 haxelib install hxjsonast --quiet
+haxelib install flixel 5.5.0
 haxelib set flixel 5.5.0 --quiet
 haxelib git lime https://github.com/GreenColdTea/lime-9.0.0
+haxelib install openfl 9.4.1
 haxelib set openfl 9.4.1
 haxelib install hxcpp --quiet
 haxelib install hxvlc --quiet --skip-dependencies
 haxelib git sl-windows-api https://github.com/GreenColdTea/windows-api-improved.git
 haxelib run lime setup flixel
+haxelib install flixel-tools 1.5.1
 haxelib set flixel-tools 1.5.1
+haxelib install flixel-ui 2.6.3
 haxelib set flixel-ui 2.6.3
+haxelib install flixel-addons 3.3.2
 haxelib set flixel-addons 3.3.2
+haxelib install hscript 2.4.0
 haxelib set hscript 2.4.0
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git
