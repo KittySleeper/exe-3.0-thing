44bca5310a97250b0e27683edd146d58f8596c5f
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-06-18 14:18:28 +0000

    workflows update

---
+++ setup/unix.sh
@@ -0,0 +1,27 @@
+@echo off
+cls
+title FNF' Vs Sonic.exe Necessary Libraries Installer
+echo.
+echo Installing necessary libraries. Please wait...
+echo.
+haxelib setup C:\haxelib
+haxelib install tjson --quiet
+haxelib install hxjsonast --quiet
+haxelib set flixel 5.5.0 --never --quiet
+haxelib git lime https://github.com/GreenColdTea/lime-9.0.0
+haxelib set openfl 9.4.1
+haxelib install hxcpp --quiet
+haxelib install hxvlc --quiet --skip-dependencies
+haxelib git sl-windows-api https://github.com/GreenColdTea/windows-api-improved.git
+haxelib run lime setup flixel
+haxelib set flixel-tools 1.5.1
+haxelib set flixel-ui 2.6.3
+haxelib set flixel-addons 3.3.2
+haxelib set hscript 2.4.0
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git
+haxelib install hxcpp-debug-server
+haxelib list
+echo.
+echo Done! Press any key to close the app!
+pause
--- setup/windows.bat
+++ setup/windows.bat
@@ -8,7 +8,7 @@ haxelib setup C:\haxelib
 haxelib install tjson --quiet
 haxelib install hxjsonast --quiet
 haxelib set flixel 5.5.0 --never --quiet
-haxelib git lime https://github.com/GreenColdTea/lime-9.0.0
+haxelib I git lime https://github.com/GreenColdTea/lime-9.0.0
 haxelib set openfl 9.4.1
 haxelib install hxcpp --quiet
 haxelib install hxvlc --quiet --skip-dependencies
@@ -20,6 +20,7 @@ haxelib set flixel-addons 3.3.2
 haxelib set hscript 2.4.0
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git
+haxelib install hxcpp-debug-server
 haxelib list
 echo.
 echo Done! Press any key to close the app!
