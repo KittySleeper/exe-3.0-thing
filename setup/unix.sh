62e0bab8a7e39a5ffc45651fec50a8e57a92cc2a
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-06-18 14:31:23 +0000

    linux/mac fix

--- setup/unix.sh
+++ setup/unix.sh
@@ -1,20 +1,19 @@
 cd ..
 echo Makking the main haxelib and setuping folder in same time..
 mkdir ~/haxelib && haxelib setup ~/haxelib
-echo FNF' Vs Sonic.exe Necessary Libraries Installer
 haxelib install tjson --quiet
 haxelib install hxjsonast --quiet
-haxelib set flixel 5.5.0 --never --quiet
+haxelib install flixel 5.5.0 --never --quiet
 haxelib git lime https://github.com/GreenColdTea/lime-9.0.0
-haxelib set openfl 9.4.1
+haxelib install openfl 9.4.1
 haxelib install hxcpp --quiet
 haxelib install hxvlc --quiet --skip-dependencies
 haxelib git sl-windows-api https://github.com/GreenColdTea/windows-api-improved.git
 haxelib run lime setup flixel
-haxelib set flixel-tools 1.5.1
-haxelib set flixel-ui 2.6.3
-haxelib set flixel-addons 3.3.2
-haxelib set hscript 2.4.0
+haxelib install flixel-tools 1.5.1
+haxelib install flixel-ui 2.6.3
+haxelib install flixel-addons 3.3.2
+haxelib install hscript 2.4.0
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git
 haxelib install hxcpp-debug-server
